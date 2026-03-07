1e3fe0805f82e3c1b559cb1020ce9d99fa6a38bd
9gj4d9jwjg-a11y <9gj4d9jwjg@privaterelay.appleid.com>
2026-03-07 17:22:26 +0100

    feat: complete Enkoji version with stable Safari layout

---
+++ src/app/[locale]/debug-version/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import OriginalOngTemplate from '@/components/OriginalOngTemplate';
+
+export default function DebugPage() {
+  return (
+    <div>
+      <h1 className="p-10 text-3xl font-bold bg-yellow-100">DEBUG: LOCAL VERSION</h1>
+      <OriginalOngTemplate />
+    </div>
+  );
+}
